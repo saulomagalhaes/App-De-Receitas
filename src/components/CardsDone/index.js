c2667987a524a2f5d8a64c5ccc37023689e571ab
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-04-29 18:58:07 -0300

    feat: requisitos 57

--- src/components/CardsDone/index.js
+++ src/components/CardsDone/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import shareIcon from '../../images/shareIcon.svg';
 
 function CardsDone() {
+  const [copied, setCopied] = useState('');
   const doneRecipes = localStorage.getItem('doneRecipes')
     ? JSON.parse(localStorage.getItem('doneRecipes'))
     : '';
+
+  const handleCopy = (url) => {
+    navigator.clipboard.writeText(url);
+    setCopied('Link copied!');
+  };
 
   if (doneRecipes === '') return <h1>Não há receitas finalizadas</h1>;
 
@@ -19,17 +25,20 @@ function CardsDone() {
             alt={ recipe.name }
           />
           <div className="card-info">
-            <img
-              alt="share icon"
-              data-testid={ `${index}-horizontal-share-btn` }
-              src={ shareIcon }
-            />
+            <button
+              type="button"
+              onClick={ () => handleCopy(`http://localhost:3000/foods/${recipe.id}`) }
+            >
+              <img
+                alt="share icon"
+                data-testid={ `${index}-horizontal-share-btn` }
+                src={ shareIcon }
+              />
+              {copied}
+            </button>
             {recipe.alcoholicOrNot === '' ? (
-              <p
-                data-testid={ `${index}-horizontal-top-text` }
-              >
+              <p data-testid={ `${index}-horizontal-top-text` }>
                 {`${recipe.nationality} - ${recipe.category}`}
-
               </p>
             ) : (
               <p data-testid={ `${index}-horizontal-top-text` }>
