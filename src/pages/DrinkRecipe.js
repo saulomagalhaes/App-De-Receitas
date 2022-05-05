72acd9dc5bd62b486559a939bbd7f2a02564ab3d
TH.ThiagoAvels <thiagoavels1@gmail.com>
2022-05-05 16:56:46 -0300

    feat: Slide Funcionando Foods/Drinks

--- src/pages/DrinkRecipe.js
+++ src/pages/DrinkRecipe.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Slider from 'react-slick';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import shareIcon from '../images/shareIcon.svg';
 import { checkLocalStorage, concatenateIngredient } from '../services/FuncRecipesDetails';
 import { getDrinkById, getFoodsByName } from '../redux/actions';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const NINETEEN_MAX_LENGTH = 19;
 
@@ -19,6 +22,13 @@ function DrinkRecipe(props) {
   const [buttonPhrase, setButtonPhrase] = useState(true);
   const [buttonProgress] = useState(false);
   const dispatch = useDispatch();
+
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+  };
 
   useEffect(() => {
     dispatch(getFoodsByName(''));
@@ -56,7 +66,7 @@ function DrinkRecipe(props) {
         {
           drinks
             .map((element) => (
-              <div key={ element.idDrink }>
+              <section key={ element.idDrink }>
                 <img
                   src={ element.strDrinkThumb }
                   alt="Imagem da Bebida"
@@ -95,40 +105,30 @@ function DrinkRecipe(props) {
                 </ul>
                 <h1>Instructions</h1>
                 <p data-testid="instructions">{element.strInstructions}</p>
-                <div
+                <Slider
+                  { ...settings }
                   data-testid="recomendation-card"
-                  id="carouselExampleSlidesOnly"
-                  className="carousel slide"
-                  data-ride="carousel"
                 >
-                  <div className="carousel-inner">
-                    {
-                      foods
-                        .splice(NINETEEN_MAX_LENGTH)
-                        .map((img, indexImg) => (
-                          <div
-                            key={ indexImg }
-                            className={ indexImg === 0
-                              ? 'carousel-item active'
-                              : 'carousel-item' }
-                          >
-                            <img
-                              data-testid={ `${indexImg}-recomendation-card` }
-                              className="d-block w-20"
-                              src={ img.strMealThumb }
-                              style={ { width: '200px', display: 'inline' } }
-                              alt="Recomendação de Comida"
-                            />
-                            <span>{img.strCategory}</span>
-                            <p data-testid={ `${indexImg}-recomendation-title` }>
-                              {img.strMeal}
-                            </p>
-                          </div>
-                        ))
-                    }
-                  </div>
-                </div>
-              </div>
+                  {
+                    foods
+                      .splice(NINETEEN_MAX_LENGTH)
+                      .map((img, indexImg) => (
+                        <div key={ indexImg }>
+                          <img
+                            data-testid={ `${indexImg}-recomendation-card` }
+                            src={ img.strMealThumb }
+                            style={ { width: '200px' } }
+                            alt="Recomendação de Comida"
+                          />
+                          <span>{img.strCategory}</span>
+                          <p data-testid={ `${indexImg}-recomendation-title` }>
+                            {img.strMeal}
+                          </p>
+                        </div>
+                      ))
+                  }
+                </Slider>
+              </section>
             ))
         }
         <button
