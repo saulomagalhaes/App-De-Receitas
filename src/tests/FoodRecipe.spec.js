b7686bdec556f922d9432a6f4af5e84755127692
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-05-05 21:50:18 -0300

    feat: Implementacao de testes

--- src/tests/FoodRecipe.spec.js
+++ src/tests/FoodRecipe.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import waitForExpect from 'wait-for-expect';
-// import userEvent from '@testing-library/user-event';
+import waitForExpect from 'wait-for-expect';
+import userEvent from '@testing-library/user-event';
 import fetchMock from '../../cypress/mocks/fetch';
 import App from '../App';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
@@ -11,7 +11,7 @@ import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 //   ROUTE_FOODS_INGREDIENTS,
 // } from './helpers/constants';
 
-describe('1 - FoodProgress', () => {
+describe('1 - Food Recipe', () => {
   beforeEach(() => {
     global.fetch = jest.fn((url) => fetchMock(url));
   });
@@ -21,13 +21,20 @@ describe('1 - FoodProgress', () => {
 
   it(`1.1 - Verifica se existe um botão de compartilhar, 
   um botão de favoritar e um botao de finalizar`, async () => {
-    renderWithRouterAndRedux(<App />, { initialEntries: ['/foods/52977/in-progress'] });
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/foods/52977'] });
     const shareBtn = await screen.findByTestId('share-btn');
     const favoriteBtn = await screen.findByTestId('favorite-btn');
-    const finishBtn = await screen.findByTestId('finish-recipe-btn');
+    const finishBtn = await screen.findByTestId('start-recipe-btn');
 
     expect(shareBtn).toBeInTheDocument();
     expect(favoriteBtn).toBeInTheDocument();
     expect(finishBtn).toBeInTheDocument();
+  });
+
+  it('1.2 - Verifica se o botão de Iniciar Receita funciona', async () => {
+    renderWithRouterAndRedux(<App />, { initialEntries: ['/foods/52977'] });
+    const startBtn = await screen.findByTestId('start-recipe-btn');
+    userEvent.click(startBtn);
+    waitForExpect(() => expect(pathname).toBe('/foods/52977/in-progress'));
   });
 });
